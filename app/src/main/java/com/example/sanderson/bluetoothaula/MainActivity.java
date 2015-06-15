e83653d61666fe558f5bb80c7f4eac23a200466d
sandeco <sandecom@gmail.com>
2015-06-14 21:28:15 -0300

    last

--- app/src/main/java/com/example/sanderson/bluetoothaula/MainActivity.java
+++ app/src/main/java/com/example/sanderson/bluetoothaula/MainActivity.java
@@ -1,18 +1,48 @@
 package com.example.sanderson.bluetoothaula;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final int REQUEST_ENABLE_BT = 100;
+    private BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(adapter==null){
+            Toast.makeText(this,"Não tem bluetooth", Toast.LENGTH_LONG).show();
+        }else{
+            isBluetoothAtivo();
+        }
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        isBluetoothAtivo();
+    }
+
+    private void isBluetoothAtivo(){
+        if (!adapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }else{
+            Toast.makeText(this,"Bluetooth está ativo",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
